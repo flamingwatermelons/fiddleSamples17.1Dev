d623bd4ced5fbb7fe38f6ab2ec820f09e2079e09
unknown <tfs1@S0106WEB1T.IGWEB.LOCAL>
2017-04-12 09:51:33 -0400

    SB build 17.1.20171.55

--- JA/HtmlSamples/grid/grouping/demo.js
+++ JA/HtmlSamples/grid/grouping/demo.js
@@ -2,22 +2,39 @@ $(function () {
 			$("#grid").igGrid({
 				autoGenerateColumns: false,
 				width: "100%",
-				height: "400px",
+				height: "500px",
 				columns: [
 					{ headerText: "注文 ID", key: "OrderID", dataType: "number", width: "0%", hidden: true },
-					{ headerText: "配送先の国", key: "ShipCountry", dataType: "string", width: "20%" },
-					{ headerText: "注文日", key: "OrderDate", dataType: "date", width: "10%" },
-					{ headerText: "単価", key: "UnitPrice", dataType: "string", width: "20%" },
-					{ headerText: "割引", key: "Discount", dataType: "string", width: "10%" },
-					{ headerText: "出荷名", key: "ShipName", dataType: "string", width: "20%" },
-					{ headerText: "配送先市町村", key: "ShipCity", dataType: "string", width: "12%" },
-					{ headerText: "顧客住所", key: "City", dataType: "string", width: "8%" }
+					{ headerText: "配送先の国", key: "ShipCountry", dataType: "string", width: "12%" },
+					{ headerText: "注文日", key: "OrderDate", dataType: "date", width: "18%" },
+					{ headerText: "単価", key: "UnitPrice", dataType: "number", width: "17%" },
+					{ headerText: "割引", key: "Discount", dataType: "number", width: "13%" },
+					{ headerText: "出荷名", key: "ShipName", dataType: "string", width: "14%" },
+					{ headerText: "配送先市町村", key: "ShipCity", dataType: "string", width: "13%" },
+					{ headerText: "顧客住所", key: "City", dataType: "string", width: "13%" }
 				],
 				dataSource: northwindInvoices,
 				features: [
 					{
 						name: 'GroupBy',
 						groupByDialogContainment: "window",
+						groupSummaries: [
+							{
+								summaryFunction: "Sum", label: "Sum = ", format: ".##"
+							},
+							{
+								summaryFunction: "Count", label: "Count = "
+							},
+							{
+								summaryFunction: "Avg", label: "Avg = ", format: ".##"
+							},
+							{
+								summaryFunction: "Min", label: "Min = "
+							},
+							{
+								summaryFunction: "Max", label: "Max = "
+							}
+						],
 						columnSettings: [
 							{
 								columnKey: "ShipCountry",
@@ -32,11 +49,11 @@ $(function () {
 										customSummary: function (data) {
 											var count = 0, date = new Date(1996, 7, 1);
 											$.map(data.array, function (d) {
-												  if (d > date) {
-													  count++;
-												  }
-											  });
-											  return count.toFixed(0);
+												if (d > date) {
+													count++;
+												}
+											});
+											return count.toFixed(0);
 										}
 									}
 								]
