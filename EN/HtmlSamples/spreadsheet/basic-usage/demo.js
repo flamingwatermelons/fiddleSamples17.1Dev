6711b9da706025fb7ff3bb2e7525e1dcbb838bde
unknown <tfs1@S0106WEB1T.IGWEB.LOCAL>
2017-04-28 02:47:58 -0400

    SB build 17.1.20171.68

--- EN/HtmlSamples/spreadsheet/basic-usage/demo.js
+++ EN/HtmlSamples/spreadsheet/basic-usage/demo.js
@@ -5,28 +5,28 @@ $.ig.loader({
 			resources: "igSpreadsheet,igExcel.LoadSaveXlsx,igEditors"
 		});
 	 
-			$(function () {
-				$("#spreadsheet").igSpreadsheet({
-					height: "600",
-					width: "100%"
-				});
-
-				var workbook = null;
-				var xhr = new XMLHttpRequest();
-				xhr.open('GET', 'http://dev.igniteui.local/17-1/data-files/statistic.xlsx', true);
-				xhr.responseType = 'arraybuffer';
-
-				xhr.onload = function (e) {
-					// response is unsigned 8 bit integer
-					var responseArray = new Uint8Array(this.response);
-					$.ig.excel.Workbook.load(responseArray, function () {
-						workbook = arguments[0];
-						$("#spreadsheet").igSpreadsheet("option", "workbook", workbook);
-					}, function () {
-						console.log("fail");
-					})
-				};
-
-				xhr.send();
+		$(function () {
+			$("#spreadsheet").igSpreadsheet({
+				height: "600",
+				width: "100%"
 			});
+
+			var workbook = null;
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', 'http://dev.igniteui.local/17-1/data-files/statistic.xlsx', true);
+			xhr.responseType = 'arraybuffer';
+
+			xhr.onload = function (e) {
+				// response is unsigned 8 bit integer
+				var responseArray = new Uint8Array(this.response);
+				$.ig.excel.Workbook.load(responseArray, function () {
+					workbook = arguments[0];
+					$("#spreadsheet").igSpreadsheet("option", "workbook", workbook);
+				}, function () {
+					console.log("fail");
+				})
+			};
+
+			xhr.send();
+		});
 });
